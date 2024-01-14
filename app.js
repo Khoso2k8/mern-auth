4edad0379a52e646ee09bff0d6324af4fadc32aa
Khoso2k8 <meerkhan2k8@gmail.com>
2024-01-14 04:02:59 -0800

    allow-cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const userRouter = require('./routes/userRoute');
@@ -7,6 +8,11 @@ const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
+const corsOptions = {
+  credentials: true,
+  origin: ['https://mern-auth-wak.vercel.app/', 'http://localhost:5173'],
+};
+app.use(cors(corsOptions));
 
 app.use('/api/v1/users', userRouter);
 
